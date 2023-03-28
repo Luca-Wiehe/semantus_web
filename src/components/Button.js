2adbf7d1d636eace2c4be2dfe1b2c91d07cd42cb
ge75kel <ge75kel@mytum.de>
2023-03-29 01:22:17 +0200

    Improved code and documentation

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,6 +4,13 @@ import React, {useState} from 'react';
 import '../styles/style.css'
 import '../styles/components/Button.css';
 
+/**
+ * A button component that is in accordance with Semantus styling scheme.
+ * @param {string} appearance - The appearance of the button. Valid options are 'primary', 'secondary', and 'tertiary'.
+ * @param {number} width - The width of the button in pixels.
+ * @param {number} height - The height of the button in pixels.
+ * @param {function} onClick - The click handler for the button.
+ */
 const Button = (props) => {
    const { appearance, width, height, onClick } = props;
    const bgColor = appearance === 'primary' ? 'var(--color_tertiary)' : (appearance === 'secondary' ? 'var(--color_primary)' : 'var(--color_secondary)');
--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -5,6 +5,9 @@ import '../styles/style.css'
 import '../styles/components/MenuBar.css';
 import Button from './Button';
 
+/**
+ * A responsive MenuBar that sticks to Semantus style guidelines.
+ */
 const MenuBar = () => {
    // state variable to check if user is logged in
    const isLoggedIn = true;
@@ -31,21 +34,22 @@ const MenuBar = () => {
     }, []);
 
    // display profile and points when logged in 
-   const LoggedInMenuItems = () => {
-      return (
-         <>
-            <li className="link-item">
-                  Mein Profil
-            </li>
-            <li className="user-item">
-               <span className="upper-span">meister_luca</span>
-               <span>2000 Points</span>
-            </li>
-            <li>
-               <img className="avatar" src="images/avatar.png" alt="avatar" />
-            </li>
-         </>
-      );
+   const LoggedInMenuItems = ({isExpanded}) => {
+      const menuItems = [
+         <li className="link-item">
+             Mein Profil
+         </li>,
+         <li className="user-item">
+            <span className="upper-span">meister_luca</span>
+            <span>2000 Points</span>
+         </li>,
+         <li>
+            <img className="avatar" src="images/avatar.png" alt="avatar" />
+         </li>
+      ]
+      
+      // profile-image is last element for menu bar but first element in expanded menu
+      return isExpanded ? menuItems.reverse() : menuItems;
    }
 
    // display login-button when logged out
@@ -66,32 +70,37 @@ const MenuBar = () => {
 
    return (
       <div className={`menu-container ${isMenuOpen && !isLargeScreen ? "full-height menu-open" : ""}`}> 
+         {/* Menu Bar */}
          <div className={`menu-bar-wrapper ${isMenuOpen && !isLargeScreen ? "menu-open" : ""}`}>
+
+            {/* Logo and 'Semantus' Title */}
             <div className="title-wrapper">
                <img className="menu-bar-logo" src="images/logo512.png" alt="Semantus Logo" />
                <span className="menu-bar-title">Semantus</span>
             </div>
+
+            {/* Menu items */}
             {isLargeScreen ?
-               (<ul className="menu-bar-items">
-                  <li className="link-item">
-                     Regeln
-                  </li>
-                  {isLoggedIn ? <LoggedInMenuItems /> : <LoggedOutMenuItems />}
-               </ul>) :
+               (
+                  <ul className="menu-bar-items">
+                     <li className="link-item">
+                        Regeln
+                     </li>
+                     {isLoggedIn ? <LoggedInMenuItems isExpanded={!isLargeScreen} /> : <LoggedOutMenuItems />}
+                  </ul>
+               ) :
                isMenuOpen ? 
                   <BiChevronsUp size="50px" color="var(--color_primary)" style={{"cursor": "pointer"}} onClick={() => setMenuOpen(!isMenuOpen)} /> :
                   <BiChevronsDown size="50px" color="var(--color_primary)" style={{"cursor": "pointer"}} onClick={() => setMenuOpen(!isMenuOpen)} />
             }
          </div>
+         {/* Expanded Menu */}
          {isMenuOpen && !isLargeScreen && 
             <ul className="expanded-menu-bar-items">
-               <li><img className="avatar" src="images/avatar.png" alt="avatar" /></li>
-               <li className="user-item">
-                  <span className="upper-span">meister_luca</span>
-                  <span>2000 Points</span>
+               {isLoggedIn ? <LoggedInMenuItems isExpanded={true} /> : <LoggedOutMenuItems />}
+               <li className="link-item">
+                  Regeln
                </li>
-               <li className="link-item">Mein Profil</li>
-               <li className="link-item">Regeln</li>
             </ul>
          }
       </div>
